b92028c1bd01b13c1cc47c53de5f84160dd87ccc
t3chnique <144436602+t3chnique@users.noreply.github.com>
2024-06-09 16:58:14 +0300

    Add files via upload

--- handlers/routes.py
+++ handlers/routes.py
@@ -4,6 +4,12 @@ from modules import modules
 
 
 def configure_routes(app):
+    # Define the maximum allowed file size in bytes (e.g., 10 MB)
+    MAX_FILE_SIZE_BYTES = 1000 * 1024 * 1024  # 1 GB
+    # Define the maximum allowed total folder size in bytes (e.g., 100 MB)
+    MAX_FOLDER_SIZE_BYTES = 10000 * 1024 * 1024  # 10 GB
+    current_date = datetime.now().strftime('%d.%m.%y')
+
     @app.route("/")
     def index():
         hello = modules.hello()
@@ -12,11 +18,6 @@ def configure_routes(app):
 
     @app.route('/status')
     def status():
-        # Define the maximum allowed file size in bytes (e.g., 10 MB)
-        MAX_FILE_SIZE_BYTES = 1000 * 1024 * 1024  # 1 GB
-        # Define the maximum allowed total folder size in bytes (e.g., 100 MB)
-        MAX_FOLDER_SIZE_BYTES = 10000 * 1024 * 1024  # 10 GB
-        current_date = datetime.now().strftime('%d.%m.%y')
         message = ""
         message += "Hello!"
         if current_date:
