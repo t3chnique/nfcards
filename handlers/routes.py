f93d80148e71aab225343302c5b74e7a66f5d50a
t3chnique <144436602+t3chnique@users.noreply.github.com>
2024-06-10 14:38:29 +0300

    Add files via upload

--- handlers/routes.py
+++ handlers/routes.py
@@ -11,6 +11,8 @@ def configure_routes(app):
     # Define the maximum allowed total folder size in bytes (e.g., 100 MB)
     MAX_FOLDER_SIZE_BYTES = 10000 * 1024 * 1024  # 10 GB
     current_date = datetime.now().strftime('%d.%m.%y')
+    ALLOWED_EXTENSIONS = {'avi', 'flv', 'wmv', 'mov', 'mp4',
+                          'm4v', 'mpeg', 'mpg', 'mkv', 'webm'}
     '''
     json_path = os.path.join(os.path.dirname(__file__), 'data', 'ascii.json')
     with open(json_path, "r", encoding="utf-8") as json_file:
@@ -39,24 +41,23 @@ def configure_routes(app):
                         f"{round(MAX_FILE_SIZE_BYTES/1000000/1000)} GB, "
                         "Folder size limit: "
                         f"{round(MAX_FOLDER_SIZE_BYTES/1000000/1000)} GB")
+        message += f"ALLOWED_EXTENSIONS: {ALLOWED_EXTENSIONS}"
         return message
 
     @app.route('/example', methods=['POST'])
     def example():
         return render_template('example.html')
 
+    '''
     def allowed_file(filename):
         # Add other popular video formats as needed
         ALLOWED_EXTENSIONS = {'avi', 'flv', 'wmv', 'mov', 'mp4',
                               'm4v', 'mpeg', 'mpg', 'mkv', 'webm'}
-        testing(ALLOWED_EXTENSIONS)
-        return '.' in filename and filename.rsplit('.',
-                                                   1)[1].lower(
-                                                       ) in ALLOWED_EXTENSIONS
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    '''
 
     @app.route('/test')
-    def testing(ALLOWED_EXTENSIONS):
-        allowed_file()
+    def testing():
         message = ""
         message += f"ALLOWED_EXTENSIONS: {ALLOWED_EXTENSIONS}"
         return message
